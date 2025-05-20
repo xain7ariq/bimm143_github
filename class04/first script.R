af7826f8795d7fd076c348ce121272d397d249bd
Muhammad Tariq <mztariq@ucsd.edu>
2025-05-20 12:00:58 -0700

    Add class 04 and 07

---
+++ class04/first script.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Muhammad Tariq"
+#' date: "April 10 2025"
+#' ---
+
+#My firts R script
+x<-1:50
+plot(x,sin(x))
+plot(x,sin(x), typ="l",col="darkblue", lwd=3,xlab="Silly x axis",ylab="sensible y axis")
